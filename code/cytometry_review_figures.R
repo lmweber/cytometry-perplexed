352bfbd1848554055912e553fa82f7b6ee0fe2ec
Lukas Weber <lukas.weber.edu@gmail.com>
2021-06-11 02:28:28 -0400

    updated figures - heatmap, facets, colors, formatting

--- code/cytometry_review_figures.R
+++ code/cytometry_review_figures.R
@@ -52,6 +52,8 @@ d_sub <- d_sub[!dups, ]
 pop_ids_sub <- pop_ids_sub[!dups]
 stopifnot(length(pop_ids_sub) == nrow(d_sub))
 
+ix_keep <- ix[!dups]
+
 
 # -------------------
 # Dimension reduction
@@ -60,7 +62,7 @@ stopifnot(length(pop_ids_sub) == nrow(d_sub))
 n_dims <- 2
 
 
-# run Rtsne with different seeds
+# run Rtsne with different random seeds
 
 set.seed(101)
 out_Rtsne_1 <- Rtsne(as.matrix(d_sub), dims = n_dims)
@@ -87,7 +89,7 @@ head(dims_Rtsne_3)
 stopifnot(nrow(dims_Rtsne_3) == length(pop_ids_sub))
 
 
-# run UMAP with different seeds
+# run UMAP with different random seeds
 
 set.seed(101)
 out_umap_1 <- umap(d_sub)
@@ -146,60 +148,74 @@ table(labels_fs)
 # Generate plots: clustering
 # --------------------------
 
-colors_clus <- c(rainbow(12))
+library(colorspace)
+set.seed(7)
+colors_clus <- sample(qualitative_hcl(12))
+
+#colors_clus <- unname(palette.colors(palette = "Polychrome 36"))
+#colors_clus <- rainbow(12)
+#library(RColorBrewer)
+#colors_clus <- brewer.pal(12, "Set3")
+#colors_clus <- brewer.pal(12, "Paired")
 
 # function for tSNE plots
-plot_tSNE_clus <- function(dims_tSNE, labels, colors) {
+plot_tSNE_clus <- function(dims_tSNE, labels, colors, 
+                           size = 0.5, alpha = 0.5) {
   
   d_plot <- cbind(as.data.frame(dims_tSNE), cluster = as.factor(labels))
   
   ggplot(d_plot, aes(x = tSNE_1, y = tSNE_2, color = cluster)) + 
-    geom_point(size = 0.5, alpha = 0.5) + 
+    geom_point(size = size, alpha = alpha) + 
     scale_color_manual(values = colors) + 
     theme_bw() + 
-    theme(panel.grid = element_blank(), 
+    theme(legend.key.size = unit(0.75, "lines"), 
+          panel.grid = element_blank(), 
           axis.text = element_blank(), 
-          axis.ticks = element_blank())
+          axis.ticks = element_blank()) + 
+    guides(color = guide_legend(override.aes = list(size = 1.5, alpha = 0.75)))
 }
 
 # seed 1
 plot_tSNE_clus(dims_Rtsne_1, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_tSNE_seed1.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_tSNE_seed1.png", width = 4, height = 3)
 
 # seed 2
 plot_tSNE_clus(dims_Rtsne_2, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_tSNE_seed2.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_tSNE_seed2.png", width = 4, height = 3)
 
 # seed 3
 plot_tSNE_clus(dims_Rtsne_3, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_tSNE_seed3.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_tSNE_seed3.png", width = 4, height = 3)
 
 
 # function for UMAP plots
-plot_UMAP_clus <- function(dims_umap, labels, colors) {
+plot_UMAP_clus <- function(dims_umap, labels, colors, 
+                           size = 0.2, alpha = 0.5) {
   
   d_plot <- cbind(as.data.frame(dims_umap), cluster = as.factor(labels))
   
   ggplot(d_plot, aes(x = UMAP_1, y = UMAP_2, color = cluster)) + 
-    geom_point(size = 0.5, alpha = 0.5) + 
+    geom_point(size = size, alpha = alpha) + 
     scale_color_manual(values = colors) + 
     theme_bw() + 
-    theme(panel.grid = element_blank(), 
+    theme(legend.key.size = unit(0.75, "lines"), 
+          panel.grid = element_blank(), 
           axis.text = element_blank(), 
-          axis.ticks = element_blank())
+          axis.ticks = element_blank()) + 
+    guides(color = guide_legend(override.aes = list(size = 1.5, alpha = 0.75)))
 }
 
 # seed 1
 plot_UMAP_clus(dims_umap_1, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_UMAP_seed1.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_UMAP_seed1.png", width = 4, height = 3)
 
 # seed 2
 plot_UMAP_clus(dims_umap_2, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_UMAP_seed2.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_UMAP_seed2.png", width = 4, height = 3)
 
 # seed 3
 plot_UMAP_clus(dims_umap_3, labels_fs, colors_clus)
-ggsave("../plots/FlowSOM_UMAP_seed3.png", width = 4.5, height = 3.5)
+ggsave("../plots/FlowSOM_UMAP_seed3.png", width = 4, height = 3)
 
 
 # ----------------------------
@@ -217,9 +233,11 @@ plot_tSNE_truth <- function(dims_tSNE, pop_ids, colors) {
     geom_point(size = 0.5, alpha = 0.5) + 
     scale_color_manual(values = colors) + 
     theme_bw() + 
-    theme(panel.grid = element_blank(), 
+    theme(legend.key.size = unit(0.75, "lines"), 
+          panel.grid = element_blank(), 
           axis.text = element_blank(), 
-          axis.ticks = element_blank())
+          axis.ticks = element_blank()) + 
+    guides(color = guide_legend(override.aes = list(size = 1.5, alpha = 0.75)))
 }
 
 # seed 1
@@ -241,12 +259,14 @@ plot_UMAP_truth <- function(dims_umap, pop_ids, colors) {
   d_plot <- cbind(as.data.frame(dims_umap), population = as.factor(pop_ids))
   
   ggplot(d_plot, aes(x = UMAP_1, y = UMAP_2, color = population)) + 
-    geom_point(size = 0.5, alpha = 0.5) + 
+    geom_point(size = 0.2, alpha = 0.5) + 
     scale_color_manual(values = colors) + 
     theme_bw() + 
-    theme(panel.grid = element_blank(), 
+    theme(legend.key.size = unit(0.75, "lines"), 
+          panel.grid = element_blank(), 
           axis.text = element_blank(), 
-          axis.ticks = element_blank())
+          axis.ticks = element_blank()) + 
+    guides(color = guide_legend(override.aes = list(size = 1.5, alpha = 0.75)))
 }
 
 # seed 1
@@ -262,19 +282,90 @@ plot_UMAP_truth(dims_umap_3, pop_ids_sub, colors_truth)
 ggsave("../plots/truth_UMAP_seed3.png", width = 6, height = 3.5)
 
 
+# facetted plot for all 3 seeds
+d_umap <- rbind(
+  cbind(as.data.frame(dims_umap_1), seed = "seed1", population = as.character(pop_ids_sub)), 
+  cbind(as.data.frame(dims_umap_2), seed = "seed2", population = as.character(pop_ids_sub)), 
+  cbind(as.data.frame(dims_umap_3), seed = "seed3", population = as.character(pop_ids_sub)))
+
+d_umap$seed <- as.factor(d_umap$seed)
+d_umap$population <- as.factor(d_umap$population)
+
+ggplot(d_umap, aes(x = UMAP_1, y = UMAP_2, color = population)) + 
+  geom_point(size = 0.1, alpha = 0.5) + 
+  scale_color_manual(values = colors_truth) + 
+  facet_wrap(~seed) + 
+  theme_bw() + 
+  theme(legend.key.size = unit(0.75, "lines"), 
+        panel.grid = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank()) + 
+  guides(color = guide_legend(override.aes = list(size = 1.5, alpha = 0.75)))
+
+ggsave("../plots/UMAP_randomseeds.png", width = 10.5, height = 3)
+
+
+# --------------------------
+# Cluster expression heatmap
+# --------------------------
+
+# use heatmap plotting function from CATALYST
+
+library(CATALYST)
+
+# format into SingleCellExperiment for CATALYST
+sce <- SingleCellExperiment(
+  rowData = colData(d_se)[colData(d_se)$marker_class == "type", ], 
+  colData = rowData(d_se)[ix_keep, ], 
+  assays = list(
+    exprs = t(d_sub)
+  )
+)
+
+colData(sce)$cluster_id <- labels_fs
+
+# structure required by plotExprHeatmap
+colData(sce)$sample_id <- "sample01"
+metadata(sce)$cluster_codes <- data.frame(som100 = factor(1:k), meta12 = factor(1:k))
+
+# order markers alphabetically
+sce <- sce[order(rowData(sce)$marker_name), ]
+
+pdf("../plots/heatmap_clusters.pdf", width = 6.25, height = 4)
+plotExprHeatmap(sce, features = "type", by = "cluster_id", 
+                k = "meta12", k_pal = colors_clus, 
+                row_clust = FALSE, col_clust = FALSE)
+dev.off()
+
+
+# alternative code
+
+# library(dplyr)
+# library(tidyr)
+# 
+# df <- cbind(as.data.frame(d_sub), cluster_id = labels_fs, cell_id = 1:nrow(d_sub)) %>% 
+#   gather("marker", "expr", -cluster_id, -cell_id) %>% 
+#   group_by(cluster_id, marker) %>% 
+#   summarize(median = median(expr)) %>% 
+#   spread(marker, median)
+# 
+# mat <- as.matrix(df[, 2:33])
+# rownames(mat) <- df$cluster_id
+
+
 # -------------------------
-# Clustering within T-cells
+# Clustering within T cells
 # -------------------------
 
-# identify T-cells from FlowSOM clustering by inspecting UMAP and heatmap
+# identify T cells from FlowSOM clustering by inspecting UMAP and heatmap
 
-# T cells are cluster 1
+# T cells are clusters 1 and 2
 table(labels_fs)
 # number and proportion of cells identified as T cells
-table(labels_fs == 1)
-mean(labels_fs == 1)
+table(labels_fs %in% c(1, 2))
+mean(labels_fs %in% c(1, 2))
 
-d_sub_tcells <- d_sub[labels_fs == 1, ]
+d_sub_tcells <- d_sub[labels_fs %in% c(1, 2), ]
 dim(d_sub_tcells)
 
 # perform second round of clustering on T cells only
@@ -285,7 +376,7 @@ out_fs_tcells <- BuildSOM(out_fs_tcells)
 out_fs_tcells <- BuildMST(out_fs_tcells)
 labels_pre_meta_tcells <- out_fs_tcells$map$mapping[, 1]
 # number of meta-clusters
-k <- 6
+k <- 12
 seed <- 123
 out_fs_tcells <- metaClustering_consensus(out_fs_tcells$map$codes, k = k, seed = seed)
 labels_fs_tcells <- out_fs_tcells[labels_pre_meta_tcells]
@@ -293,24 +384,30 @@ labels_fs_tcells <- out_fs_tcells[labels_pre_meta_tcells]
 table(labels_fs_tcells)
 
 
-# plot T cell subsets: tSNE
+# calculate tSNE and UMAP dimension reduction: T cells
 
-dims_Rtsne_1_tcells <- dims_Rtsne_1[labels_fs == 1, ]
-stopifnot(nrow(dims_Rtsne_1_tcells) == length(labels_fs_tcells))
+n_dims <- 2
 
-colors_clus <- rainbow(6)
+set.seed(100)
+out_Rtsne <- Rtsne(as.matrix(d_sub_tcells), dims = n_dims)
+dims_Rtsne <- out_Rtsne$Y
+colnames(dims_Rtsne) <- c("tSNE_1", "tSNE_2")
+stopifnot(nrow(dims_Rtsne) == length(labels_fs_tcells))
 
-plot_tSNE_clus(dims_Rtsne_1_tcells, labels_fs_tcells, colors_clus)
-ggsave("../plots/FlowSOM_tSNE_seed1_Tcells.png", width = 4.5, height = 3.5)
+set.seed(100)
+out_umap_tcells <- umap(d_sub_tcells)
+dims_umap_tcells <- out_umap_tcells$layout
+colnames(dims_umap_tcells) <- c("UMAP_1", "UMAP_2")
+stopifnot(nrow(dims_umap_tcells) == length(labels_fs_tcells))
 
 
-# plot T cell subsets: UMAP
+# plot T cell subsets: tSNE and UMAP
 
-dims_umap_1_tcells <- dims_umap_1[labels_fs == 1, ]
-stopifnot(nrow(dims_umap_1_tcells) == length(labels_fs_tcells))
+colors_tcells <- qualitative_hcl(k)
 
-colors_clus <- rainbow(6)
+plot_tSNE_clus(dims_Rtsne, labels_fs_tcells, colors_tcells)
+ggsave("../plots/Tcells_FlowSOM_tSNE.png", width = 4.5, height = 3.5)
 
-plot_UMAP_clus(dims_umap_1_tcells, labels_fs_tcells, colors_clus)
-ggsave("../plots/FlowSOM_UMAP_seed1_Tcells.png", width = 4.5, height = 3.5)
+plot_UMAP_clus(dims_umap_tcells, labels_fs_tcells, colors_tcells)
+ggsave("../plots/Tcells_FlowSOM_UMAP.png", width = 4.5, height = 3.5)
 
